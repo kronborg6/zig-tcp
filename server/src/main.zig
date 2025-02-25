c8caf5ee4794079de192c77848a767a787d0a936
Kronborg <mkronborg7@gmail.com>
2025-02-25 22:25:25 +0100

    yes

--- server/src/main.zig
+++ server/src/main.zig
@@ -1,8 +1,15 @@
+// Multithreaded server with a ThreadPool
 const std = @import("std");
 const net = std.net;
 const posix = std.posix;
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var pool: std.Thread.Pool = undefined;
+    try std.Thread.Pool.init(&pool, .{ .allocator = allocator, .n_jobs = 64 });
+
     const address = try std.net.Address.parseIp("127.0.0.1", 5882);
 
     const tpe: u32 = posix.SOCK.STREAM;
@@ -13,49 +20,114 @@ pub fn main() !void {
     try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
     try posix.bind(listener, &address.any, address.getOsSockLen());
     try posix.listen(listener, 128);
-    var buf: [128]u8 = undefined;
+
     while (true) {
         var client_address: net.Address = undefined;
         var client_address_len: posix.socklen_t = @sizeOf(net.Address);
         const socket = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
-            // Rare that this happens, but in later parts we'll
-            // see examples where it does.
             std.debug.print("error accept: {}\n", .{err});
             continue;
         };
-        defer posix.close(socket);
 
-        std.debug.print("{} connected\n", .{client_address});
+        const client = Client{ .socket = socket, .address = client_address };
+        try pool.spawn(Client.handle, .{client});
+    }
+}
+
+const Client = struct {
+    socket: posix.socket_t,
+    address: std.net.Address,
+
+    fn handle(self: Client) void {
+        defer posix.close(self.socket);
+        self._handle() catch |err| switch (err) {
+            error.Closed => {},
+            error.WouldBlock => {}, // read or write timeout
+            else => std.debug.print("[{any}] client handle error: {}\n", .{ self.address, err }),
+        };
+    }
+
+    fn _handle(self: Client) !void {
+        const socket = self.socket;
+        std.debug.print("[{}] connected\n", .{self.address});
 
         const timeout = posix.timeval{ .sec = 2, .usec = 500_000 };
-        // our existing read timeout
         try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.RCVTIMEO, &std.mem.toBytes(timeout));
-        // our write timeout
         try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.SNDTIMEO, &std.mem.toBytes(timeout));
 
-        const read = posix.read(socket, &buf) catch |err| {
-            std.debug.print("error reading: {}\n", .{err});
-            continue;
-        };
+        var buf: [1024]u8 = undefined;
+        var reader = Reader{ .pos = 0, .buf = &buf, .socket = socket };
 
-        if (read == 0) {
-            continue;
+        while (true) {
+            const msg = try reader.readMessage();
+            std.debug.print("[{}] sent: {s}\n", .{ self.address, msg });
+        }
+    }
+};
+
+const Reader = struct {
+    buf: []u8,
+    pos: usize = 0,
+    start: usize = 0,
+    socket: posix.socket_t,
+
+    fn readMessage(self: *Reader) ![]u8 {
+        var buf = self.buf;
+
+        while (true) {
+            if (try self.bufferedMessage()) |msg| {
+                return msg;
+            }
+            const pos = self.pos;
+            const n = try posix.read(self.socket, buf[pos..]);
+            if (n == 0) {
+                return error.Closed;
+            }
+            self.pos = pos + n;
+        }
+    }
+
+    fn bufferedMessage(self: *Reader) !?[]u8 {
+        const buf = self.buf;
+        const pos = self.pos;
+        const start = self.start;
+
+        std.debug.assert(pos >= start);
+        const unprocessed = buf[start..pos];
+        if (unprocessed.len < 4) {
+            self.ensureSpace(4 - unprocessed.len) catch unreachable;
+            return null;
         }
 
-        write(socket, buf[0..read]) catch |err| {
-            // This can easily happen, say if the client disconnects.
-            std.debug.print("error writing: {}\n", .{err});
-        };
-    }
-}
+        const message_len = std.mem.readInt(u32, unprocessed[0..4], .little);
 
-fn write(socket: posix.socket_t, msg: []const u8) !void {
-    var pos: usize = 0;
-    while (pos < msg.len) {
-        const written = try posix.write(socket, msg[pos..]);
-        if (written == 0) {
-            return error.Closed;
+        // the length of our message + the length of our prefix
+        const total_len = message_len + 4;
+
+        if (unprocessed.len < total_len) {
+            try self.ensureSpace(total_len);
+            return null;
         }
-        pos += written;
+
+        self.start += total_len;
+        return unprocessed[4..total_len];
     }
-}
+
+    fn ensureSpace(self: *Reader, space: usize) error{BufferTooSmall}!void {
+        const buf = self.buf;
+        if (buf.len < space) {
+            return error.BufferTooSmall;
+        }
+
+        const start = self.start;
+        const spare = buf.len - start;
+        if (spare >= space) {
+            return;
+        }
+
+        const unprocessed = buf[start..self.pos];
+        std.mem.copyForwards(u8, buf[0..unprocessed.len], unprocessed);
+        self.start = 0;
+        self.pos = unprocessed.len;
+    }
+};
